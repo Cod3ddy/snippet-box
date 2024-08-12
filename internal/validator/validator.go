6f4a305ab9a18246803f7d1a4ff2072a57bf15cd
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-13 00:20:31 +0200

    feat: add new validation helper func AddNonFieldErrors

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -9,6 +9,7 @@ import (
 
 type Validator struct {
 	FieldErrors map[string]string
+	NonFieldErrors [] string
 }
 
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
@@ -16,7 +17,7 @@ var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) ==0
 }
 
 // Add an error messahe to the FieldErrors map (if an entry does not exist for a given key)
@@ -65,3 +66,7 @@ func Matches(value string, rx *regexp.Regexp) bool{
 	return rx.MatchString(value)
 }
 
+// Add error messages to the new NonFieldErrors slice
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
