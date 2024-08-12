a86eb7c9b17fa4949f458749bca0857620d14859
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-12 08:11:53 +0200

    feat: add dynamic route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,10 +14,12 @@ func (app *application) routes() http.Handler {
 	//using the mux.Handle() to register the file server as the hande for all the URL paths that start with "/static/" . For matching paths, we strip the "/static" before the request reaches the file server.
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	dynamic := alice.New(app.SessionManger.LoadAndSave)
+
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// middleware chain containt 'standard' middleware.
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
