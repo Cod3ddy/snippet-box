9d3434796994573aeaeb5be997e45abf541c800d
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-12 21:19:15 +0200

    fix: typo

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,13 +1,11 @@
 package validator
 
 import (
-	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
 
-var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string
@@ -50,15 +48,4 @@ func MaxChars(value string, n int) bool{
 // PermittedValue returns true if a value is in a list of specific permitted value
 func PermittedValue[T comparable](value T, permittedValues ...T)bool{
 	return slices.Contains(permittedValues, value)
-}
-
-// // MinChars() returns true if a value contains no more than n characters
-
-func MinChars (value string, n int)  bool{
-	return utf8.RuneCountInString(value) >= n
-}
-
-// Matches() returns true if a value matches a provided compiled regular expression
-func Matches(value string, rx *regexp.Regexp) bool{
-	return rx.MatchString(value)
 }
