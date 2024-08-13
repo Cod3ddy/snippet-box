bd4884eba7a59715875de9f0693600ac169c0774
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-13 14:24:37 +0200

    feat: serve static files from embedded fs

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/Cod3ddy/snippet-box/ui"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// using the mux.Handle() to register the file server as the hande for all the URL paths that start with "/static/" . For matching paths, we strip the "/static" before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	// serve static files from embed fs
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.SessionManger.LoadAndSave,  noSurf, app.Authenicate)
 
