5deb29de3c5dba592513f9509ee6d93f549461db
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-13 21:53:54 +0200

    feat: implement test cases for snippet view handlers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/Cod3ddy/snippet-box/internal/assert"
 )
 
-func TestPing(t *testing.T){
+func TestPing(t *testing.T) {
 	app := newTestApplication(t)
 
 	ts := newTestServer(t, app.routes())
@@ -17,4 +17,66 @@ func TestPing(t *testing.T){
 
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
+}
+
+func TestSnippetView(t *testing.T) {
+	// Instance of app struct
+	app := newTestApplication(t)
+
+	// Test Server
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	// table-driven tests to check the responses sent by our
+	// application for different URLs.
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name: "Valid ID",
+			urlPath: "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond...",
+		},
+		{
+			name: "Non-Existent ID",
+			urlPath: "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Negative ID",
+			urlPath: "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Decimal ID",
+			urlPath: "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "String ID",
+			urlPath: "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name: "Empty ID",
+			urlPath: "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests{
+		t.Run(tt.name, func(t *testing.T){
+			code, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantBody != ""{
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
 }
