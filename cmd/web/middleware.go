fed27a117b11092e94c76f1178f2bdf0e81665c8
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-08-13 12:22:53 +0200

    feat: implement the noSurf middleware using justians/nosurf package

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func commonHeaders(next http.Handler) http.Handler {
@@ -60,4 +62,18 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler{
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+
+// Uses a customized CSRF cookie with the Secure, Path and HttpOnly Attiributes set
+
+func noSurf(next http.Handler) http.Handler{
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: true,
+	})
+
+	return csrfHandler
 }
